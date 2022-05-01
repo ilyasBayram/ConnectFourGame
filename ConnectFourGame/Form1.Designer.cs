763e6c26c7879864aeeba0c7ab8b64ca187bad46
ilyasBayram <ilyas.bayram@hotmail.com>
2022-05-01 12:50:34 +0300

    board style is added. Player color choose is added.

--- ConnectFourGame/Form1.Designer.cs
+++ ConnectFourGame/Form1.Designer.cs
@@ -29,22 +29,105 @@ namespace ConnectFourGame
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnBlue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBlue.Location = new System.Drawing.Point(28, 218);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(127, 64);
+            this.btnBlue.TabIndex = 0;
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRed.ForeColor = System.Drawing.Color.Black;
+            this.btnRed.Location = new System.Drawing.Point(28, 433);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(127, 64);
+            this.btnRed.TabIndex = 1;
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.Location = new System.Drawing.Point(46, 168);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(0, 20);
+            this.lblBlue.TabIndex = 2;
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Location = new System.Drawing.Point(46, 384);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(0, 20);
+            this.lblRed.TabIndex = 3;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMessage.Location = new System.Drawing.Point(23, 91);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(176, 29);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "Choose a color";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(294, 121);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(924, 738);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Tan;
-            this.ClientSize = new System.Drawing.Size(1155, 677);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1324, 945);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Label lblBlue;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- ConnectFourGame/Form1.cs
+++ ConnectFourGame/Form1.cs
@@ -18,33 +18,48 @@ namespace ConnectFourGame
             InitializeComponent();
         }
 
-        private void RoundButton()
+        string player ="";
+       
+        private void PlayerChoose()
         {
-            
+            if (player=="Blue")
+            {
+                lblBlue.Text = "Player1";
+                lblRed.Text = "Player2";
+            }
+            else
+            {
+                lblBlue.Text = "Player2";
+                lblRed.Text = "Player1";
+            }
+            btnBlue.Enabled = false;
+            btnRed.Enabled = false;
+            lblMessage.Visible = false;
         }
         private void Form1_Load(object sender, EventArgs e)
         {
             
             int countRow = 0;
-            for (int i = 1; i < 6; i++)
+            for (int i = 1; i < 8; i++)
             {
                 int countColom = 0;
-                for (int j = 1; j < 6; j++)
+                for (int j = 1; j < 7; j++)
                 { 
                     
                     CreateButton button = new CreateButton();
-                    button.Size = new Size(60, 60);
+                    button.Size = new Size(58, 58);
                     button.BackColor = Color.Azure;
-                    button.Location = new System.Drawing.Point(200+countRow, 80+countColom);
+                    button.Location = new System.Drawing.Point(250+countRow, 100+countColom);
                     button.FlatAppearance.BorderSize = 0;
                     button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
                     button.FlatAppearance.BorderColor = Color.Black;
-                    button.ForeColor = Color.White;
+                    button.ForeColor = Color.Black;
                     button.Click += Button_Click;
                     this.Controls.Add(button);
-                    countColom += 90;
+                    button.BringToFront();
+                    countColom += 70;
                 }
-                countRow += 120;
+                countRow += 75;
 
             }
             
@@ -52,11 +67,31 @@ namespace ConnectFourGame
 
         private void Button_Click(object sender, EventArgs e)
         {
-            if ((sender as Button).BackColor==Color.Azure)
+            if (player == "Red")
             {
                 (sender as Button).BackColor = Color.Red;
                 (sender as Button).Enabled = false;
+                player = "Blue";
             }
+            else if (player == "Blue")
+            {
+                (sender as Button).BackColor = Color.GreenYellow;
+                (sender as Button).Enabled = false;
+                player = "Red";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player = "Blue";
+            PlayerChoose();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player = "Red";
+            PlayerChoose();
         }
     }
     public class CreateButton : Button
